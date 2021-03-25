b5b6696aa55db4a61c1937c1cf7e3ef5ef88c78c
silencelee <lij311@qq.com>
2021-03-26 00:10:38 +0800

    init noncopyable

---
+++ base/noncopyable.h
@@ -0,0 +1,19 @@
+#ifndef SILENCELIB_BASE_NONCOPYABLE_H
+#define SILENCELIB_BASE_NONCOPYABLE_H
+
+namespace silence
+{
+
+class noncopyable {
+public:
+	noncopyable(const noncopyable&) = delete;
+	void operator=(const noncopyable&) = delete;
+
+protected:
+	noncopyable() = default;
+	~noncopyalbe() = default;
+};
+
+}
+
+#endif
